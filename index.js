0694b9de1d227c4d36e139836d2005f5768749d3
OlegArtemiev <oartemjev@gmail.com>
2021-11-26 17:50:16 +0200

    try to fix hexlet check     TypeError: formatters[format] is not a function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import genDiffString from './src/jsonDiffBuilder.js';
 import getObjectFromFile from './src/readFile.js';
 
-export default (filePath1, filePath2, format) => {
+export default (filePath1, filePath2, format = 'stylish') => {
   const object1 = getObjectFromFile(filePath1);
   const object2 = getObjectFromFile(filePath2);
   const diffString = genDiffString(object1, object2, format);
